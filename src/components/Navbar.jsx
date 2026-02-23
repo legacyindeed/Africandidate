6111bb07278c5a3fb5105ecc7a12583c081f545c
legacyindeed <ibrahim.shardow@outlook.com>
2026-02-23 18:28:04 -0500

    Make logo much bigger to occupy half the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -140,7 +140,7 @@ export default function Navbar() {
         }
 
         .logo-img {
-          height: 70px;
+          height: 120px;
           width: auto;
           object-fit: contain;
         }
